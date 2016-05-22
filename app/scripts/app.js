b54b2a3faa333a8cfd9cebce7c708cfc62cc812b
Sam Moorhouse <sam.j.moorhouse@gmail.com>
2016-05-22 08:53:49 -0700

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name turntableApp
+ * @description
+ * # turntableApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('turntableApp', []);
