3b310caa41e00e3fa063908031d0c267ead11b8d
Sam Moorhouse <sam.j.moorhouse@gmail.com>
2016-06-22 11:49:35 +0700

    cruft

--- server.js
+++ server.js
@@ -26,18 +26,13 @@ app.use(stormpath.init(app, {
 
 var port = process.env.PORT || 8080;
 var mongoose = require('mongoose');
-var passport = require('passport');
-var flash = require('connect-flash');
 
 var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var session = require('express-session');
 
 // configuration ===============================================================
 mongoose.connect(process.env.mongodb_connectionURL); // connect to our database
-
-require('./config/passport')(passport); // pass passport for configuration
 
 //js,css
 app.use('/js', express.static('js'));
@@ -58,16 +53,8 @@ app.use(bodyParser()); // get information from html forms
 
 app.set('view engine', 'ejs'); // set up ejs for templating
 
-// required for passport
-app.use(session({
-  secret: 'ilovescotchscotchyscotchscotch'
-})); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
-
 // routes ======================================================================
-require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+require('./app/routes.js')(app); // load our routes and pass in our app and fully configured passport
 
 // launch ======================================================================
 app.on('stormpath.ready', function() {
