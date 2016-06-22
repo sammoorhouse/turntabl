b899c9fbadd6e8b05f55156da444c55573dd0bf2
Sam Moorhouse <sam.j.moorhouse@gmail.com>
2016-06-22 12:51:27 +0700

    login modal

--- js/login-register.js
+++ js/login-register.js
@@ -7,20 +7,7 @@
  *
  */
 
-function showLoginForm(){
-    $('#loginModal .registerBox').fadeOut('fast',function(){
-        $('.loginBox').fadeIn('fast');
-        $('.register-footer').fadeOut('fast',function(){
-            $('.login-footer').fadeIn('fast');
-        });
-
-        $('.modal-title').html('Login with');
-    });
-     $('.error').removeClass('alert alert-danger').html('');
-}
-
 function openLoginModal(){
-    showLoginForm();
     setTimeout(function(){
         $('#loginModal').modal('show');
     }, 230);
