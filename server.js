4d3c48212c1656dcb0845ca47a03dbc9cc877dd9
Sam Moorhouse <sam.j.moorhouse@gmail.com>
2016-06-22 14:35:48 +0700

    cruft

--- server.js
+++ server.js
@@ -3,10 +3,7 @@ require('dotenv').config({
   silent: true
 }); //for dev. In production, variables are in the environment.
 
-// set up ======================================================================
-// get all the tools we need
 var express = require('express');
-
 var stormpath = require('express-stormpath');
 
 var app = express();
@@ -34,13 +31,11 @@ app.use(stormpath.init(app, {
 
 var port = process.env.PORT || 8080;
 var mongoose = require('mongoose');
-
 var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-// configuration ===============================================================
-mongoose.connect(process.env.mongodb_connectionURL); // connect to our database
+mongoose.connect(process.env.mongodb_connectionURL);
 
 //js,css
 app.use('/js', express.static('js'));
@@ -53,18 +48,12 @@ app.use('/bower_components', express.static('bower_components'));
 app.use('/fonts', express.static('fonts'));
 app.use('/img', express.static('img'));
 
+//app.use(morgan('dev'));
+app.use(bodyParser());
+app.set('view engine', 'ejs');
 
-// set up our express application
-app.use(morgan('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
+require('./app/routes.js')(app);
 
-app.set('view engine', 'ejs'); // set up ejs for templating
-
-// routes ======================================================================
-require('./app/routes.js')(app); // load our routes and pass in our app and fully configured passport
-
-// launch ======================================================================
 app.on('stormpath.ready', function() {
   app.listen(port);
   console.log('The magic happens on port ' + port);
