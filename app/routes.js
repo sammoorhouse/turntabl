7a4575b60d3b5292035d095d6edf81d3817b0b14
Sam Moorhouse <sam.j.moorhouse@gmail.com>
2016-06-23 14:52:46 +0700

    typo

--- app/routes.js
+++ app/routes.js
@@ -7,161 +7,162 @@ var typeformVersionString = 'v0.4'
   //var typeformVersionString = 'latest'
 
 module.exports = function(app) {
-    app.get('/', function(req, res) {
-      var user = req.user
-      if (typeof user != "undefined") {
-        console.log("USEREMAIL: " + user.email)
-      }
-      res.render('index.ejs', {
-        user: user
-      });
-    });
-
-    // create-event SECTION =========================
-    app.get('/create-event', stormpath.loginRequired, function(req, res) {
-        var user = req.user
-        console.log("USEREMAIL: " + user.email)
-
-        //generate typeform
-        var typeformUrl = "https://api.typeform.io/" + typeformVersionString + "/forms"
-        console.log('typeformUrl: ' + typeformUrl)
-        var formData = generateForm(user)
-        console.log("data: " + JSON.stringify(formData, null, 2))
-        request.debug=true
-        request.post({
-            url: typeformUrl,
-            json: formData,
-            headers: {
-              "X-API-TOKEN": process.env.TYPEFORM_APIKEY
-            },
-            function(err, resp) {
-              if (!err && resp.statusCode == 200) {
-                console.log('typeform Upload successful: ' + JSON.stringify(resp.body, null, 2));
-                var formLink = resp.body['links'].find(function(el) {
-                  el.rel === "form_render"
-                }).href
-                console.log('typeform url: ' + formLink)
-
-                res.render('create-event.ejs', {
-                  formUrl: body['_links'],
-                  user: user
-                });
-              } else if (err) {
-                return console.error('typeform upload failed:', err);
-              } else {
-                return console.error('typeform upload failed:', resp.body);
-              }
-            })
-        })
-
-      app.post('/form/create-event', stormpath.loginRequired, function(req, res) {
-        var newEvent = new Event();
-        var id = generateID()
-        var user = req.user
-        var leaderEmail = user.email
-
-        newEvent.id = id
-        newEvent.name = req.eventTitle
-        newEvent.starts = req.eventDateTime
-        newEvent.durationMins = 60
-        newEvent.leader = leaderEmail
-        newEvent.client = req.eventClient
-        newEvent.clientPaid = false
-        newEvent.leaderPaid = false
-        newEvent.attended = false
-        newEvent.eventValue = req.eventValue
-
-        //create openTok session
-        opentok.createSession(function(err, session) {
-          if (err) {
-            console.log("sessionId creation error: " + err)
-            throw err;
-          } else {
-            console.log("sessionId: " + session.sessionId)
-            newEvent.openTokSessionId = session.sessionId
-
-            newEvent.save(function(err) {
-              if (err) {
-                throw err;
-                res.redirect('/')
-              } else {
-                console.log("newEvent.id = " + newEvent.id)
-                res.redirect('/event/' + newEvent.id)
-              }
-            });
-          }
-        });
-      })
-
-      app.get('/event/:id', stormpath.loginRequired, function(req, res) {
-        var evtId = req.param('id')
-        var fakeclient = req.param('fakeclient')
-        console.log("evtId: " + evtId)
-        Event.findOne({
-          'id': evtId
-        }, function(err, event) {
-          // if there are any errors, return the error
-          if (err) {
-            console.log("err: " + err)
-            res.redirect('/');
-          }
-
-          // if no event is found, return the message
-          if (!event) {
-            console.log("no evt")
-            res.redirect('/');
-          }
-          // all is well, render event
-          else {
-            var user = req.user
-            var userEmail = user.email
-            var sessionId = event.openTokSessionId
-            var token = opentok.generateToken(sessionId)
-            console.log("USEREMAIL: " + user.email)
-            if ((event.leader === userEmail) && (!fakeclient)) { // it's the leader
-              return res.render('event.ejs', {
-                user: user,
-                event: event,
-                apiKey: process.env.tokboxAuth_apiKey,
-                sessionId: sessionId,
-                token: token
-              });
-            } else {
-              var eventValue = event.eventValue
-              return res.render('event-client.ejs', {
-                user: req.user,
-                event: event,
-                apiKey: process.env.tokboxAuth_apiKey,
-                sessionId: sessionId,
-                token: token,
-                eventValue: eventValue
-              });
-            }
-          }
-        });
-      })
-
-      function generateForm(user) {
-        console.log('generating user form for ' + user.givenName)
-        var formData = {
-          "title": "My first typeform",
-          "fields": [{
-            "type": "short_text",
-            "question": "What is your name?"
-          }]
-        }
-
-        console.log("data after generation: " + formData)
-        return formData
-      }
-
-      function generateID() {
-        var ALPHABET = '23456789abdegjkmnpqrvwxyz';
-        var ID_LENGTH = 8;
-        var rtn = '';
-        for (var i = 0; i < ID_LENGTH; i++) {
-          rtn += ALPHABET.charAt(Math.floor(Math.random() * ALPHABET.length));
-        }
-        return rtn;
-      }
+  app.get('/', function(req, res) {
+    var user = req.user
+    if (typeof user != "undefined") {
+      console.log("USEREMAIL: " + user.email)
     }
+    res.render('index.ejs', {
+      user: user
+    });
+  });
+
+  // create-event SECTION =========================
+  app.get('/create-event', stormpath.loginRequired, function(req, res) {
+    var user = req.user
+    console.log("USEREMAIL: " + user.email)
+
+    //generate typeform
+    var typeformUrl = "https://api.typeform.io/" + typeformVersionString + "/forms"
+    console.log('typeformUrl: ' + typeformUrl)
+    var formData = generateForm(user)
+    console.log("data: " + JSON.stringify(formData, null, 2))
+    request.debug = true
+    request.post({
+        url: typeformUrl,
+        json: formData,
+        headers: {
+          "X-API-TOKEN": process.env.TYPEFORM_APIKEY
+        }
+      },
+      function(err, resp) {
+        if (!err && resp.statusCode == 200) {
+          console.log('typeform Upload successful: ' + JSON.stringify(resp.body, null, 2));
+          var formLink = resp.body['links'].find(function(el) {
+            el.rel === "form_render"
+          }).href
+          console.log('typeform url: ' + formLink)
+
+          res.render('create-event.ejs', {
+            formUrl: body['_links'],
+            user: user
+          });
+        } else if (err) {
+          return console.error('typeform upload failed:', err);
+        } else {
+          return console.error('typeform upload failed:', resp.body);
+        }
+      })
+  })
+
+  app.post('/form/create-event', stormpath.loginRequired, function(req, res) {
+    var newEvent = new Event();
+    var id = generateID()
+    var user = req.user
+    var leaderEmail = user.email
+
+    newEvent.id = id
+    newEvent.name = req.eventTitle
+    newEvent.starts = req.eventDateTime
+    newEvent.durationMins = 60
+    newEvent.leader = leaderEmail
+    newEvent.client = req.eventClient
+    newEvent.clientPaid = false
+    newEvent.leaderPaid = false
+    newEvent.attended = false
+    newEvent.eventValue = req.eventValue
+
+    //create openTok session
+    opentok.createSession(function(err, session) {
+      if (err) {
+        console.log("sessionId creation error: " + err)
+        throw err;
+      } else {
+        console.log("sessionId: " + session.sessionId)
+        newEvent.openTokSessionId = session.sessionId
+
+        newEvent.save(function(err) {
+          if (err) {
+            throw err;
+            res.redirect('/')
+          } else {
+            console.log("newEvent.id = " + newEvent.id)
+            res.redirect('/event/' + newEvent.id)
+          }
+        });
+      }
+    });
+  })
+
+  app.get('/event/:id', stormpath.loginRequired, function(req, res) {
+    var evtId = req.param('id')
+    var fakeclient = req.param('fakeclient')
+    console.log("evtId: " + evtId)
+    Event.findOne({
+      'id': evtId
+    }, function(err, event) {
+      // if there are any errors, return the error
+      if (err) {
+        console.log("err: " + err)
+        res.redirect('/');
+      }
+
+      // if no event is found, return the message
+      if (!event) {
+        console.log("no evt")
+        res.redirect('/');
+      }
+      // all is well, render event
+      else {
+        var user = req.user
+        var userEmail = user.email
+        var sessionId = event.openTokSessionId
+        var token = opentok.generateToken(sessionId)
+        console.log("USEREMAIL: " + user.email)
+        if ((event.leader === userEmail) && (!fakeclient)) { // it's the leader
+          return res.render('event.ejs', {
+            user: user,
+            event: event,
+            apiKey: process.env.tokboxAuth_apiKey,
+            sessionId: sessionId,
+            token: token
+          });
+        } else {
+          var eventValue = event.eventValue
+          return res.render('event-client.ejs', {
+            user: req.user,
+            event: event,
+            apiKey: process.env.tokboxAuth_apiKey,
+            sessionId: sessionId,
+            token: token,
+            eventValue: eventValue
+          });
+        }
+      }
+    });
+  })
+
+  function generateForm(user) {
+    console.log('generating user form for ' + user.givenName)
+    var formData = {
+      "title": "My first typeform",
+      "fields": [{
+        "type": "short_text",
+        "question": "What is your name?"
+      }]
+    }
+
+    console.log("data after generation: " + formData)
+    return formData
+  }
+
+  function generateID() {
+    var ALPHABET = '23456789abdegjkmnpqrvwxyz';
+    var ID_LENGTH = 8;
+    var rtn = '';
+    for (var i = 0; i < ID_LENGTH; i++) {
+      rtn += ALPHABET.charAt(Math.floor(Math.random() * ALPHABET.length));
+    }
+    return rtn;
+  }
+}
