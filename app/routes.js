d78a1df3c74f2b5c43958b7dc20460d77bc10636
Sam Moorhouse <sam.j.moorhouse@gmail.com>
2016-06-24 16:07:29 +0700

    MILLIS

--- app/routes.js
+++ app/routes.js
@@ -170,7 +170,7 @@ console.log('form submission complete')
     var eventId = req.body.eventId
     pusher.trigger("presence-event-" + eventId, 'begin',
     {
-      startTime: Date.now()
+      startTimeMillis: Date.now()
     })
     res.end()
   })
