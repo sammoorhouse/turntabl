2a9f850b6ca4ac7f3b8b127e8e94efb95fd39656
Sam Moorhouse <sam.j.moorhouse@gmail.com>
2016-06-28 18:43:59 +0700

    previewtemplate

--- js/app.js
+++ js/app.js
@@ -17,11 +17,12 @@ $(function() { //on load
       interval: false
     })
     //dropzone
-    // Get the template HTML and remove it from the doumenthe template HTML and remove it from the doument
-    //var previewNode = document.querySelector("#dropzone-template");
-    //previewNode.id = "";
-    //var previewTemplate = previewNode.parentNode.innerHTML;
-    //previewNode.parentNode.removeChild(previewNode);
+
+  // Get the template HTML and remove it from the document
+  var previewNode = document.querySelector("#template");
+  previewNode.id = "";
+  var previewTemplate = previewNode.parentNode.innerHTML;
+  previewNode.parentNode.removeChild(previewNode);
 
   var myDropzone = new Dropzone('footer', {
     url: s3Bucket, // Set the url
@@ -29,7 +30,7 @@ $(function() { //on load
     thumbnailWidth: 150,
     thumbnailHeight: 150,
     parallelUploads: 20,
-    //previewTemplate: previewTemplate,
+    previewTemplate: previewTemplate,
     acceptedMimeTypes: "image/bmp,image/gif,image/jpg,image/jpeg,image/png",
     autoQueue: false, // Make sure the files aren't queued until manually added
     previewsContainer: ".dropzone-previews", // Define the container to display the previews
