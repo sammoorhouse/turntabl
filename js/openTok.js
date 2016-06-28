87ceb856f350dcc3c8a0c6fe0b5dab2989ad49d4
Sam Moorhouse <sam.j.moorhouse@gmail.com>
2016-06-28 12:27:10 +0700

    text-chat

--- js/openTok.js
+++ js/openTok.js
@@ -4,6 +4,25 @@
 var session = TB.initSession(openTokSessionId);
 var publisher = TB.initPublisher(openTokApiKey, 'publisher');
 
+// Receive a message and append it to the history
+var msgHistory = document.querySelector('#history');
+var msgTxt = document.querySelector('#msgTxt');
+var form = document.querySelector('form#text-chat');
+
+// Send a signal once the user enters data in the form
+form.addEventListener('submit', function(event) {
+event.preventDefault();
+
+session.signal({
+    type: 'msg',
+    data: msgTxt.value
+  }, function(error) {
+    if (!error) {
+      msgTxt.value = '';
+    }
+  });
+});
+
 // Attach event handlers
 session.on({
   sessionConnected: function(event) {
@@ -11,14 +30,17 @@ session.on({
   },
   streamCreated: function(event) {
     var subContainer = document.createElement('div');
-    subContainer.id = 'stream-' + event.stream.streamId;
+    subContainer.id = 'stream-'  event.stream.streamId;
     document.getElementById('subscribers').appendChild(subContainer);
     session.subscribe(event.stream, subContainer);
   },
   signal: function(event){
-    console.log("Signal sent from connection " + event.from.id);
+       var msg = document.createElement('p');
+      msg.innerHTML = event.data;
+      msg.className = event.from.connectionId === session.connection.connectionId ? 'mine' : 'theirs';
+      msgHistory.appendChild(msg);
+      msg.scrollIntoView();
   }
-
 });
 
 // Connect to the Session using the 'apiKey' of the application and a 'token' for permission
