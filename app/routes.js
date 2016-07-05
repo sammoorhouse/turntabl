244a84f73b51dc20e9c7cba56dce78d71a9c1a44
Sam Moorhouse <sam.j.moorhouse@gmail.com>
2016-07-05 10:06:11 +0700

    inspect

--- app/routes.js
+++ app/routes.js
@@ -81,7 +81,7 @@ module.exports = function(app) {
   app.post('/form/create-event', function(formSubmissionRequest, formSubmissionResponse) {
 
     console.log("form submission webhook invoked")
-
+    console.log(util.inspect(formSubmissionRequest))
     var formId = formSubmissionRequest.body.uid;
     console.log("form id: " + formId)
     var newEvent = new Event();
