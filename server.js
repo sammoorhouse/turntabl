3edc37b81842ec6a560fabc10952e996aff5d584
Sam Moorhouse <sam.j.moorhouse@gmail.com>
2016-07-09 14:36:10 +0800

    cruft

--- server.js
+++ server.js
@@ -46,7 +46,6 @@ app.use('/font-awesome', express.static('font-awesome'));
 app.use('/fonts', express.static('fonts'));
 app.use('/img', express.static('img'));
 
-//app.use(morgan('dev'));
 app.use(bodyParser)
 app.use(busboy());
 
