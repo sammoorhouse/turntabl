5f6aa8bd9aab149980cf9fc48791fa1b7ce9a6cf
Sam Moorhouse <sam.j.moorhouse@gmail.com>
2016-07-29 15:41:34 +0000

    remove image loading bumpf

--- static/js/app.js
+++ static/js/app.js
@@ -180,30 +180,3 @@ function tick() {
   var timeRemainingHuman = moment.duration(timeRemainingMillis).humanize()
   $('#countdownClock').text(timeRemainingHuman)
 }
-
-function resizeImage(url, width, height, callback, file) {
-  var sourceImage = new Image();
-  sourceImage.crossOrigin = "anonymous"
-
-  sourceImage.onload = (function (f) {
-    return function (evt) {
-      var canvas = document.createElement("canvas");
-      canvas.width = width;
-      canvas.height = height;
-
-      if (sourceImage.width == sourceImage.height) {
-        canvas.getContext("2d").drawImage(sourceImage, 0, 0, width, height);
-      } else {
-        minVal = Math.min(sourceImage.width, sourceImage.height);
-        if (sourceImage.width > sourceImage.height) {
-          canvas.getContext("2d").drawImage(sourceImage, (sourceImage.width - minVal) / 2, 0, minVal, minVal, 0, 0, width, height);
-        } else {
-          canvas.getContext("2d").drawImage(sourceImage, 0, (sourceImage.height - minVal) / 2, minVal, minVal, 0, 0, width, height);
-        }
-      }
-      callback(canvas.toDataURL(), f);
-    }
-  })(file);
-
-  sourceImage.src = url;
-}
