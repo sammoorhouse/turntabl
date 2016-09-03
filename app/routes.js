807e5edbedd466751e3a3e8817bbdc90d4379ad0
Sam Moorhouse <sam.j.moorhouse@gmail.com>
2016-09-03 13:14:53 +0800

    stringify body

--- app/routes.js
+++ app/routes.js
@@ -297,7 +297,7 @@ module.exports = function (app, log, stormpathApp) {
 
   app.post('/stripe/update', function (req, res) {
     console.log("STRIPE UPDATE RECEIVED")
-    console.log(res.body)
+    console.log(JSON.stringufy(res.body))
     res.writeHead(200);
     res.end()
   })
