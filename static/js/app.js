c70fadaff928c7316acf35d96df5324629af1c97
hmily <missjumping@gmail.com>
2016-09-29 23:45:31 -0400

    profile page + modal

--- static/js/app.js
+++ static/js/app.js
@@ -5,7 +5,6 @@ var nanobar
 
 $(function () { //on load
 
-   
     $(".dropdown-toggle").dropdown();
     $('.dropdown-menu > li').click(function() {
     var toggle = $(this).parent().siblings('.dropdown-toggle');
@@ -21,6 +20,34 @@ $('#sessionName').keyup(function() {
   
   $('#count_message').html(text_remaining + " / " + text_max);
 });
+
+
+//modal
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+//btn.onclick = function() {
+    modal.style.display = "block";
+//}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
     
 
 
